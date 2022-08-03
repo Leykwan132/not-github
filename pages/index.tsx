0f0ca4106ff5e4e8f3be9f3ddb105ba4e2884139
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-04 00:04:12 +0800

    first version of navbar UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
-  return <div>let's go?</div>;
+  return <div></div>;
 };
 
 export default Home;
