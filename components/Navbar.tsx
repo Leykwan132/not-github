6158667d0b52e79f2dfcbbafe205009e35a5ab34
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-06 17:23:48 +0800

    Github authentication system implemented with toast notification system

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { BellIcon } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import DropDownPlus from "./Navbar/components/DropDownPlus";
 import DropDownAcc from "./Navbar/components/DropDownAcc";
-
+import { signIn, useSession } from "next-auth/react";
+import toast from "react-hot-toast";
 const Navbar = () => {
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    if (status === "authenticated") {
+      toast.success("Logged in successfully!");
+    } else if (status === "unauthenticated") {
+      toast.success("Logged out successfully!");
+    }
+  }, [status]);
+
   return (
     <div className="flex justify-between bg-gray-900 px-4 py-3">
       {/* Front-part */}
@@ -41,12 +51,19 @@ const Navbar = () => {
 
       {/* End part */}
       <div className="mr-3 flex items-center space-x-3">
+        {!session && (
+          <p
+            onClick={() => signIn()}
+            className="cursor-pointer rounded-md border border-gray-800 bg-green-700 py-1 px-2 text-sm text-white hover:bg-green-800"
+          >
+            Sign In
+          </p>
+        )}
         <div className="cursor-pointer text-white ">
           <BellIcon className="h-6 w-6 hover:text-gray-400" />
         </div>
         <DropDownPlus />
-
-        <DropDownAcc />
+        {session && <DropDownAcc />}
       </div>
     </div>
   );
