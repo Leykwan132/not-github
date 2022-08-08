545a1869557b6c71b49ae9fdf63f8ee2133817a3
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-08 21:03:15 +0800

    Left part of the homepage UI Implemented.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
+import { useState } from "react";
+import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+
 import {
   BookOpenIcon,
   CodeIcon,
   CollectionIcon,
+  PuzzleIcon,
+  QrcodeIcon,
 } from "@heroicons/react/outline";
-import type { NextPage } from "next";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { SiTailwindcss, SiTypescript, SiJavascript } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+
 import CardList from "../components/Homepage/components/CardList";
 
 const Home: NextPage = () => {
@@ -18,10 +25,54 @@ const Home: NextPage = () => {
     <div className="grid grid-cols-3 gap-4 px-5 pt-10 lg:px-28 ">
       {/* Left component */}
       <div className="flex flex-col">
-        <p className="text-white">123</p>
-        {/* <div className="relative h-12 w-12">
-          <Image src={session?.user?.image} objectFit="contain" layout="fill" />
-        </div> */}
+        <Image
+          src={session?.user?.image}
+          objectFit="contain"
+          width="300px"
+          height="300px"
+        />
+        <p className="mt-4 ml-14 truncate text-left text-2xl font-bold text-gray-300">
+          Ley Kwan Choo
+        </p>
+        <p className="ml-14 mt-1 text-left font-sans text-gray-400">
+          lchoo2@wisc.edu
+        </p>
+        <p className="mt-3 ml-14 text-left text-lg text-gray-400">
+          Bachelor's in Data Science, Class of 2024
+        </p>
+        <div className="ml-14 mt-2 flex space-x-4  text-gray-400">
+          <div className="flex items-center space-x-1">
+            <PuzzleIcon className="h-5 w-5" />
+            <p className="font-extrabold text-blue-300">6</p>
+            <p>following</p>
+          </div>
+
+          <div className="flex items-center space-x-1">
+            <QrcodeIcon className="h-5 w-5" />
+            <p className="font-extrabold text-yellow-200">9</p>
+            <p>following</p>
+          </div>
+        </div>
+        <div className="ml-14 mt-12 flex flex-col text-gray-300">
+          <p className="text-md font-sans font-semibold">Built with</p>
+          <div className="mt-2 flex space-x-2">
+            <div>
+              <a href="https://www.typescriptlang.org/" target={"_blank"}>
+                <SiTypescript className="built-with-logo  hover:text-blue-400" />
+              </a>
+            </div>
+            <div>
+              <a href="https://nextjs.org/" target={"_blank"}>
+                <TbBrandNextjs className="built-with-logo  hover:text-slate-400" />
+              </a>
+            </div>
+            <div>
+              <a href="https://tailwindcss.com/" target={"_blank"}>
+                <SiTailwindcss className="built-with-logo  hover:text-sky-200" />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       {/* Right component */}
       <div className="col-span-2 flex flex-col ">
