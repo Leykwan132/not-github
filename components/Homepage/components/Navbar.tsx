4cae894b3936c25be5329e0bd4a2851b3a7704a0
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-10 23:53:52 +0800

    Navbar File Restructured.

---
+++ components/Homepage/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import {
+  BookOpenIcon,
+  CodeIcon,
+  CollectionIcon,
+} from "@heroicons/react/outline";
+import React from "react";
+
+type Props = {
+  handleUnderline: Function;
+  underlined: String;
+};
+
+const Navbar = ({ handleUnderline, underlined }: Props) => {
+  return (
+    <div className="flex items-center space-x-3  border-b border-gray-600 pb-2">
+      <div
+        onClick={() => handleUnderline("Overview")}
+        className="relative flex cursor-pointer items-center space-x-1 rounded-md py-2 px-2 text-sm font-semibold text-gray-400 hover:bg-gray-800"
+      >
+        <BookOpenIcon className="h-5 w-5" />
+        <p>Overview</p>
+        <div
+          className={`absolute right-[0px] top-[43px] h-[2px] w-full rounded-md bg-orange-500 ${
+            underlined == "Overview" ? "block" : "hidden"
+          }`}
+        />
+      </div>
+
+      <div
+        onClick={() => handleUnderline("Repo")}
+        className="relative flex cursor-pointer items-center space-x-1 rounded-md py-2 px-2 text-sm font-semibold text-gray-400 hover:bg-gray-800"
+      >
+        <CodeIcon className="h-5 w-5" />
+        <p>Repositories</p>
+        <div
+          className={`absolute right-[0px] top-[43px] h-[2px] w-full rounded-md bg-orange-500 ${
+            underlined == "Repo" ? "block" : "hidden"
+          }`}
+        />{" "}
+      </div>
+      <div
+        onClick={() => handleUnderline("Projects")}
+        className="relative flex cursor-pointer items-center space-x-1 rounded-md py-2 px-2 text-sm font-semibold text-gray-400 hover:bg-gray-800"
+      >
+        <CollectionIcon className="h-5 w-5" />
+        <p>Projects</p>
+        <div
+          className={`absolute right-[0px] top-[43px] h-[2px] w-full rounded-md bg-orange-500 ${
+            underlined == "Projects" ? "block" : "hidden"
+          }`}
+        />{" "}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ import GithubFloatSide from "../components/Homepage/components/GithubFloatSide";
 import PeaceOutFloatSide from "../components/Homepage/components/PeaceOutFloatSide";
 import ContributionCalendar from "../components/Homepage/components/ContributionCalendar";
 import ActivityOverview from "../components/Homepage/components/ActivityOverview";
+import Navbar from "../components/Homepage/components/Navbar";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
@@ -26,7 +27,6 @@ const Home: NextPage = () => {
   const handleUnderline = (item: string) => {
     setUnderlined(item);
   };
-
   return (
     <div className="relative grid grid-cols-3 gap-4 px-5 pt-10 lg:px-28 ">
       {/* Linkedin, Github link */}
@@ -87,45 +87,7 @@ const Home: NextPage = () => {
       </div>
       {/* Right component */}
       <div className="col-span-2 flex flex-col ">
-        <div className="flex items-center space-x-3  border-b border-gray-600 pb-2">
-          <div
-            onClick={() => handleUnderline("Overview")}
-            className="relative flex cursor-pointer items-center space-x-1 rounded-md py-2 px-2 text-sm font-semibold text-gray-400 hover:bg-gray-800"
-          >
-            <BookOpenIcon className="h-5 w-5" />
-            <p>Overview</p>
-            <div
-              className={`absolute right-[0px] top-[43px] h-[2px] w-full rounded-md bg-orange-500 ${
-                underlined == "Overview" ? "block" : "hidden"
-              }`}
-            />
-          </div>
-
-          <div
-            onClick={() => handleUnderline("Repo")}
-            className="relative flex cursor-pointer items-center space-x-1 rounded-md py-2 px-2 text-sm font-semibold text-gray-400 hover:bg-gray-800"
-          >
-            <CodeIcon className="h-5 w-5" />
-            <p>Repositories</p>
-            <div
-              className={`absolute right-[0px] top-[43px] h-[2px] w-full rounded-md bg-orange-500 ${
-                underlined == "Repo" ? "block" : "hidden"
-              }`}
-            />{" "}
-          </div>
-          <div
-            onClick={() => handleUnderline("Projects")}
-            className="relative flex cursor-pointer items-center space-x-1 rounded-md py-2 px-2 text-sm font-semibold text-gray-400 hover:bg-gray-800"
-          >
-            <CollectionIcon className="h-5 w-5" />
-            <p>Projects</p>
-            <div
-              className={`absolute right-[0px] top-[43px] h-[2px] w-full rounded-md bg-orange-500 ${
-                underlined == "Projects" ? "block" : "hidden"
-              }`}
-            />{" "}
-          </div>
-        </div>
+        <Navbar handleUnderline={handleUnderline} underlined={underlined} />
         <div className="mt-6 flex flex-col text-gray-400">
           <h1 className="mb-3">Popular repositories</h1>
           {/* CardList component */}
