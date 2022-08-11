3cba72ee5beefb32376c090b87d006d8a57f94f2
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-12 00:01:16 +0800

    Flex wrap added.

--- components/Homepage/components/Repo/RepoPage.tsx
+++ components/Homepage/components/Repo/RepoPage.tsx
@@ -7,7 +7,7 @@ const RepoPage = (props: Props) => {
   return (
     <div className="mt-5 flex flex-col">
       {/* SearchBar */}
-      <div className="flex justify-between">
+      <div className="flex flex-wrap justify-between">
         {/* 1 input */}
         <input
           type="text"
@@ -15,7 +15,7 @@ const RepoPage = (props: Props) => {
           placeholder="Find a repository..."
         />
         {/* 4 button */}
-        <div className="flex">
+        <div className="flex flex-wrap space-x-4">
           <div className="flex space-x-1">
             <div className="flex cursor-pointer items-center justify-center space-x-1 rounded-md border border-gray-700 bg-gray-800 px-4 py-1 font-semibold text-gray-300">
               <p className="text-sm">Type</p>
@@ -30,7 +30,7 @@ const RepoPage = (props: Props) => {
               <ArrowDownIcon className="h-3 w-3" />
             </div>
           </div>
-          <div className="ml-4 flex  cursor-pointer items-center justify-center space-x-1 rounded-md border border-gray-700 bg-green-700 px-4 py-1 font-semibold text-white">
+          <div className="flex  cursor-pointer items-center justify-center space-x-1 rounded-md border border-gray-700 bg-green-700 px-4 py-1 font-semibold text-white">
             <BookmarkAltIcon className="h-4 w-4" />
             <p className="text-sm">New</p>
           </div>
