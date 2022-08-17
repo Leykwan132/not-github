efae3610acb16c56d22633e730e8cd71ec4a52a7
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-17 17:59:36 +0800

    homepage update for no session.

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,65 +23,72 @@ const Home: NextPage = () => {
     setUnderlined(item);
   };
   const { data, error } = useQuery(GET_ALL_USERS);
-  console.log(data);
+
   return (
-    <div className="relative grid grid-cols-3 gap-4 px-5 pt-10 lg:px-28 ">
+    <div
+      className={`relative px-5 pt-10 lg:px-28 ${
+        session ? "grid grid-cols-3 gap-4 " : "flex flex-col"
+      }`}
+    >
       {/* Linkedin, Github link */}
       <LinkedinFloatSide />
       <GithubFloatSide />
       <PeaceOutFloatSide />
 
       {/* Left component */}
-      <div className="flex flex-col">
-        <Image
-          src={session?.user?.image}
-          objectFit="contain"
-          width="300px"
-          height="300px"
-        />
-        <p className="mt-4 ml-14 truncate text-left text-2xl font-bold text-gray-300">
-          Ley Kwan Choo
-        </p>
-        <p className="ml-14 mt-1 truncate text-left font-sans text-gray-400">
-          lchoo2@wisc.edu
-        </p>
-        <p className="mt-3 ml-14 text-left text-lg text-gray-400">
-          Bachelor's in Data Science, Class of 2024
-        </p>
-        <div className="ml-14 mt-2 flex flex-wrap items-center  text-gray-400">
-          <div className="mr-3 flex items-center space-x-1">
-            <PuzzleIcon className="h-5 w-5" />
-            <p className="font-extrabold text-blue-300">6</p>
-            <p>following</p>
-          </div>
+      {session && (
+        <div className="flex flex-col">
+          <Image
+            src={session?.user?.image}
+            objectFit="contain"
+            width="300px"
+            height="300px"
+          />
+          <p className="mt-4 ml-14 truncate text-left text-2xl font-bold text-gray-300">
+            Ley Kwan Choo
+          </p>
+          <p className="ml-14 mt-1 truncate text-left font-sans text-gray-400">
+            lchoo2@wisc.edu
+          </p>
+          <p className="mt-3 ml-14 text-left text-lg text-gray-400">
+            Bachelor's in Data Science, Class of 2024
+          </p>
+          <div className="ml-14 mt-2 flex flex-wrap items-center  text-gray-400">
+            <div className="mr-3 flex items-center space-x-1">
+              <PuzzleIcon className="h-5 w-5" />
+              <p className="font-extrabold text-blue-300">6</p>
+              <p>following</p>
+            </div>
 
-          <div className="flex items-center space-x-1">
-            <QrcodeIcon className="h-5 w-5" />
-            <p className="font-extrabold text-yellow-200">9</p>
-            <p>following</p>
-          </div>
-        </div>
-        <div className="ml-14 mt-12 flex flex-col text-gray-300">
-          <p className="text-md font-sans font-semibold">Built with</p>
-          <div className="mt-2 flex space-x-2">
-            <div>
-              <a href="https://www.typescriptlang.org/" target={"_blank"}>
-                <SiTypescript className="built-with-logo  hover:text-blue-500" />
-              </a>
-            </div>
-            <div>
-              <a href="https://nextjs.org/" target={"_blank"}>
-                <TbBrandNextjs className="built-with-logo  hover:text-slate-400" />
-              </a>
-            </div>
-            <div>
-              <a href="https://tailwindcss.com/" target={"_blank"}>
-                <SiTailwindcss className="built-with-logo  hover:text-sky-300" />
-              </a>
+            <div className="flex items-center space-x-1">
+              <QrcodeIcon className="h-5 w-5" />
+              <p className="font-extrabold text-yellow-200">9</p>
+              <p>following</p>
             </div>
           </div>
+          <div className="ml-14 mt-12 flex flex-col text-gray-300">
+            <p className="text-md font-sans font-semibold">Built with</p>
+            <div className="mt-2 flex space-x-2">
+              <div>
+                <a href="https://www.typescriptlang.org/" target={"_blank"}>
+                  <SiTypescript className="built-with-logo  hover:text-blue-500" />
+                </a>
+              </div>
+              <div>
+                <a href="https://nextjs.org/" target={"_blank"}>
+                  <TbBrandNextjs className="built-with-logo  hover:text-slate-400" />
+                </a>
+              </div>
+              <div>
+                <a href="https://tailwindcss.com/" target={"_blank"}>
+                  <SiTailwindcss className="built-with-logo  hover:text-sky-300" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
+
       {/* Right component */}
       <div className="col-span-2 flex flex-col ">
         <HomepageNavigate
