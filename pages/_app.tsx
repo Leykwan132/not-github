575a34ac5b1de833a95923220c56ffb0ee80c134
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-22 21:49:45 +0800

    Timeago implementation.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,14 @@ import Navbar from "../components/Navbar/components/Navbar";
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
 import { RecoilRoot } from "recoil";
+import TimeAgo from "javascript-time-ago";
+
+import en from "javascript-time-ago/locale/en.json";
+import ru from "javascript-time-ago/locale/ru.json";
+
+TimeAgo.addDefaultLocale(en);
+TimeAgo.addLocale(ru);
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ApolloProvider client={client}>
